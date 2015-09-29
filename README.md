6f8d165a53047155b4af8e6abbda24bbe6e28b17
Matt McKegg <matt@wetsand.co.nz>
2015-09-29 19:17:40 +1300

    add default init
    closes #2

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Great for use with [npm-install](https://atom.io/packages/npm-install).
 
 ## Keyboard Shortcuts
 
-- `Ctrl`+`Alt`+`N`: initialize empty package.json file
+- `Ctrl`+`Alt`+`N`: initialize default package.json file
--- index.js
+++ index.js
@@ -1,22 +1,53 @@
+var path = require('path')
+
 module.exports = {
   activate: function (state) {
-    atom.commands.add('atom-workspace', 'npm-init:empty', initEmpty)
+    atom.commands.add('atom-workspace', 'npm-init:empty', initEmpty),
+    atom.commands.add('atom-workspace', 'npm-init:default', initDefault)
   }
 }
 
 function initEmpty () {
-  var fs = require('fs')
-  var path = require('path')
+  init({
+    dependencies: {}
+  })
+}
+
+function initDefault () {
+  var projectName = path.basename(getProjectPath())
+  init({
+    name: projectName,
+    version: '0.0.0',
+    description: '',
+    main: 'index.js',
+    scripts: {
+      test: 'echo "Error: no test specified" && exit 1'
+    },
+    repository: {
+      type: 'git',
+      url: ''
+    },
+    keywords: [],
+    author: '',
+    license: 'MIT',
+    dependencies: {},
+    devDependencies: {}
+  })
+}
+
+function getProjectPath() {
   var editor = atom.workspace.getActiveTextEditor()
   var projectPath = atom.project.getPaths()[0]
-  var dirname = editor ? path.dirname(editor.getPath()) : projectPath
-  var packagePath = path.join(dirname, 'package.json')
+  return editor ? path.dirname(editor.getPath()) : projectPath
+}
+
+function init (packageInfo) {
+  var fs = require('fs')
+  var packagePath = path.join(getProjectPath(), 'package.json')
 
   fs.exists(packagePath, function (exists) {
     if (!exists) {
-      fs.writeFile(packagePath, JSON.stringify({
-        dependencies: {}
-      }, null, 2), function (err) {
+      fs.writeFile(packagePath, JSON.stringify(packageInfo, null, 2), function (err) {
         if (err) throw err
         // success
       })
